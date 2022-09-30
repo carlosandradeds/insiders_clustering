e27054666c59429bb8968b684700db1a7f6e6940
carlosandradeds <engenheiro.carlosandrade@gmail.com>
2022-09-29 21:51:19 -0300

    fix run_model

--- run_model.sh
+++ run_model.sh
@@ -1,4 +1,6 @@
 # variable
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
+source insiders/bin/activate
+
 papermill /home/ubuntu/insiders_clustering/src/models/c10.0.cmaq-Deploy.ipynb /home/ubuntu/insiders_clustering/reports/output-deploy-$data.ipynb
